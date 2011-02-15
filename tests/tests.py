30ec197bf404d902aabaf35ee2df596b420d26f2
James Hanlon <jameswhanlon@gmail.com>
2011-02-15 18:20:44 +0000

    Added sequential n-queens implementation, non-naive needs completing to allow O(1) validation of a board. Also seems to be a problem with on_chain_threaded_2 on 64 cores: an instruction is becoming corrupted.

--- tests/tests.py
+++ tests/tests.py
@@ -172,25 +172,25 @@ class FeatureTests(unittest.TestCase):
     def test_feature_on_chain_threaded_2_64(self):
         self.feature('on_chain_threaded_2', '', ['-n', '64'])
 
-    # on_chain_threaded_4
-    #def test_feature_on_chain_threaded_3_4(self):
-    #    self.feature('on_chain_threaded_3', '', ['-n', '4'])
+    # on_chain_threaded_3
+    def test_feature_on_chain_threaded_3_4(self):
+        self.feature('on_chain_threaded_3', '', ['-n', '4'])
 
-    #def test_feature_on_chain_threaded_3_16(self):
-    #    self.feature('on_chain_threaded_3', '', ['-n', '16'])
+    def test_feature_on_chain_threaded_3_16(self):
+        self.feature('on_chain_threaded_3', '', ['-n', '16'])
 
-    #def test_feature_on_chain_threaded_3_64(self):
-    #    self.feature('on_chain_threaded_3', '', ['-n', '64'])
+    def test_feature_on_chain_threaded_3_64(self):
+        self.feature('on_chain_threaded_3', '', ['-n', '64'])
 
     # on_chain_threaded_8
-    #def test_feature_on_chain_threaded_8_4(self):
-    #    self.feature('on_chain_threaded_8', '', ['-n', '4'])
+    def test_feature_on_chain_threaded_8_4(self):
+        self.feature('on_chain_threaded_8', '', ['-n', '4'])
 
-    #def test_feature_on_chain_threaded_8_16(self):
-    #    self.feature('on_chain_threaded_8', '', ['-n', '16'])
+    def test_feature_on_chain_threaded_8_16(self):
+        self.feature('on_chain_threaded_8', '', ['-n', '16'])
 
-    #def test_feature_on_chain_threaded_8_64(self):
-    #    self.feature('on_chain_threaded_8', '', ['-n', '64'])
+    def test_feature_on_chain_threaded_8_64(self):
+        self.feature('on_chain_threaded_8', '', ['-n', '64'])
 
 if __name__ == '__main__':
     if init():
